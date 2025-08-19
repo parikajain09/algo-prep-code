42719eb2b5542b01b7e7a5a831ae91198fa31c24
parikajain09 <jainparika26@gmail.com>
2025-08-19 22:05:03 +0530

    Added SumOfArray program.

---
+++ 0_1 Arrays/SumOfArray.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class SumOfArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
+        long sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        
+        System.out.println(sum);
+    }
+}
